dc1fb7f387ebac35e064532abb05411877b4882c
Prof. Thiago G. Traue <t.traue@gmail.com>
2022-03-09 22:00:41 -0300

    Create 5_loops.dart

---
+++ Aprendendo Dart/5_loops.dart
@@ -0,0 +1,17 @@
+void main() {
+  
+  ////Quando você precisa rodar um trecho de código N vezes, você utiliza um loop
+  //Loops: existem 4 tipos:
+  //for, while, do..while e foreach
+  //aqui vamos aprender por enquanto apenas os 3 primeiros
+  
+  //for:
+  //sintaze: for(<condição de inicio>; <condição de parada>; <condição de incremento)
+  for(int i = 1; i < 11; i++) {
+    print("For: $i");
+  }
+  
+  print("");
+  
+  //paramos aqui.. falta fazer o while e o do..while..
+}
