5e1e579301de98f0297aafef2c9cc5246dc3ed26
Prof. Thiago G. Traue <t.traue@gmail.com>
2022-04-27 21:58:42 -0300

    Continuação do projeto

--- uni_clima/lib/screens/home.dart
+++ uni_clima/lib/screens/home.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:dropdown_search/dropdown_search.dart';
+  import 'package:http/http.dart' as http;
 
 class Home extends StatefulWidget {
   const Home({ Key? key }) : super(key: key);
@@ -9,11 +10,75 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  
+  final List<String> _cidades = [
+    "Aracaju",
+    "Belém",
+    "Belo Horizonte",
+    "Boa Vista",
+    "Brasilia",
+    "Campo Grande",
+    "Cuiaba", 
+    "Curitiba",
+    "Florianópolis",
+    "Fortaleza",
+    "Goiânia",
+    "João Pessoa",
+    "Macapá",
+    "Maceio",
+    "Manaus",
+    "Natal",
+    "Palmas",
+    "Porto Alegre",
+    "Porto Velho",
+    "Recife",
+    "Rio Branco",
+    "Rio de Janeiro",
+    "Salvador",
+    "São Luis",
+    "São Paulo",
+    "Teresina",
+    "Vitória"
+  ];
+
+  String _cidadeSelecionada = "São Paulo";
+
+
+  carregaClima() async {
+    //variáveis auxiliares para montagem da URL de requisição
+    const String _apiURL = "api.openweathermap.org";
+    const String _path = "/data/2.5/weather";
+    const String _appid = ""; //SUA CHAVE DE API!!!
+    const String _units = "metric";
+    const String _lang = "pt_br";
+
+    //montagem dos parâmetros da URL...:
+    final _parms = {
+      "q": _cidadeSelecionada,
+      "appid": _appid,
+      "units": _units,
+      "lang": _lang
+    };
+
+    //requisição à API...
+    final climaResponse = 
+      await http.get(Uri.https(_apiURL, _path, _parms));
+
+    //apenas para depuração...
+    print("URL Montada: " + climaResponse.request!.url.toString());
+
+    //paramos aqui.... :(
+  }
+
+  
   @override
   Widget build(BuildContext context) {
+
+    double height = MediaQuery.of(context).size.height;
+
     return Scaffold(
       appBar: AppBar(
-        title: const Text("UniClima"),
+        title: Text(_cidadeSelecionada),
         centerTitle: true,
       ),
       body: Center(
@@ -21,7 +86,15 @@ class _HomeState extends State<Home> {
           children: [
             DropdownSearch<String>(
               mode: Mode.MENU,
-              //paramos aqui... :(
+              items: _cidades,
+              showSelectedItems: true,
+              showSearchBox: true,
+              maxHeight: height - 50,
+              onChanged: (value) {
+                setState(() {
+                  _cidadeSelecionada = value!;
+                });
+              },
             )
           ],
         ),
